8b7085409c00a2c6207e46d3d4d019586860724d
Roberto Sonck <rsonckjr@gmail.com>
2021-12-26 11:37:02 -0600

    Adding breakpoint support

--- src/progress_bar_exercise/components/Button/Button.js
+++ src/progress_bar_exercise/components/Button/Button.js
@@ -1,9 +1,13 @@
 import React from "react";
 import "./Button.scss";
 
-const Button = ({ children, variant = "primary", ...props }) => {
+
+// Types should be handled by ts or prop-types
+// variant: 'primary' | 'danger'
+// size: 'm' | 's'
+const Button = ({ children, variant = "primary", size="m", ...props }) => {
   return (
-    <button {...props} className={`btn ${variant}`}>
+    <button {...props} className={`btn ${variant} size-${size}`}>
       {children}
     </button>
   );
--- src/progress_bar_exercise/components/ProgressBar/ProgressBar.test.js
+++ src/progress_bar_exercise/components/ProgressBar/ProgressBar.test.js
@@ -3,40 +3,85 @@ import { render, screen, waitFor } from "@testing-library/react";
 import ProgressBar from "./ProgressBar";
 
 describe("ProgressBar", () => {
-  it("Renders hidden when loading false", () => {
-    render(<ProgressBar loading={false} />);
-    const progressBar = screen.getByTestId("progress-bar");
-    expect(progressBar.className).toBe("progress-bar hidden");
+  describe("No breakPoints", () => {
+    it("Renders hidden when loading false", () => {
+      render(<ProgressBar loading={false} />);
+      const progressBar = screen.getByTestId("progress-bar");
+      expect(progressBar.className).toBe("progress-bar hidden");
+    });
+
+    it("Animates when loading true", async () => {
+      const expectedSeconds = 15;
+      render(<ProgressBar loading expectedSeconds={expectedSeconds} />);
+
+      const progressBar = screen.getByTestId("progress-bar");
+      const progressBarInner = screen.getByTestId("progress-bar-inner");
+
+      expect(progressBar.className).toBe("progress-bar visible");
+      expect(progressBarInner.className).toBe("progress-bar__inner ");
+      expect(progressBarInner.style.width).toBe("0%");
+
+      await waitFor(() => expect(progressBarInner).toHaveClass("normal-step"));
+      expect(progressBarInner.style.width).toBe("90%");
+    });
+
+    it("Animates when loading true and when back to false", async () => {
+      const expectedSeconds = 15;
+      const { rerender } = render(
+        <ProgressBar loading expectedSeconds={expectedSeconds} />
+      );
+
+      const progressBarInner = screen.getByTestId("progress-bar-inner");
+
+      await waitFor(() => expect(progressBarInner).toHaveClass("normal-step"));
+      expect(progressBarInner.style.width).toBe("90%");
+
+      rerender(
+        <ProgressBar loading={false} expectedSeconds={expectedSeconds} />
+      );
+      expect(progressBarInner).toHaveClass("done");
+      expect(progressBarInner.style.width).toBe("100%");
+    });
   });
 
-  it("Animates when loading true", async () => {
-    const expectedSeconds = 15;
-    render(<ProgressBar loading expectedSeconds={expectedSeconds} />);
+  describe("Breakpoints", () => {
+    it("Works with breakpoints", async () => {
+      render(
+        <ProgressBar loading expectedSeconds={3} breakpoints={[20, 50]} />
+      );
 
-    const progressBar = screen.getByTestId("progress-bar");
-    const progressBarInner = screen.getByTestId("progress-bar-inner");
+      const progressBar = screen.getByTestId("progress-bar");
+      const progressBarInner = screen.getByTestId("progress-bar-inner");
 
-    expect(progressBar.className).toBe("progress-bar visible");
-    expect(progressBarInner.className).toBe("progress-bar__inner ");
-    expect(progressBarInner.style.width).toBe("0%");
+      expect(progressBar.className).toBe("progress-bar visible");
+      expect(progressBarInner.className).toBe("progress-bar__inner ");
+      expect(progressBarInner.style.width).toBe("0%");
 
-    await waitFor(() => expect(progressBarInner).toHaveClass("normal-step"));
-    expect(progressBarInner.style.width).toBe("90%");
-  });
+      await waitFor(() => expect(progressBarInner.style.width).toBe("20%"));
+      await waitFor(() => expect(progressBarInner.style.width).toBe("50%"));
+      await waitFor(() => expect(progressBarInner.style.width).toBe("90%"));
+    });
 
-  it("Animates when loading true and when back to false", async () => {
-    const expectedSeconds = 15;
-    const { rerender } = render(
-      <ProgressBar loading expectedSeconds={expectedSeconds} />
-    );
+    it("Works with breakpoints not sorted and higher than 100", async () => {
+      render(
+        <ProgressBar
+          loading
+          expectedSeconds={3}
+          breakpoints={[80, 50, 20, 100]}
+        />
+      );
 
-    const progressBarInner = screen.getByTestId("progress-bar-inner");
+      const progressBar = screen.getByTestId("progress-bar");
+      const progressBarInner = screen.getByTestId("progress-bar-inner");
 
-    await waitFor(() => expect(progressBarInner).toHaveClass("normal-step"));
-    expect(progressBarInner.style.width).toBe("90%");
+      expect(progressBar.className).toBe("progress-bar visible");
+      expect(progressBarInner.className).toBe("progress-bar__inner ");
+      expect(progressBarInner.style.width).toBe("0%");
 
-    rerender(<ProgressBar loading={false} expectedSeconds={expectedSeconds} />);
-    expect(progressBarInner).toHaveClass("done");
-    expect(progressBarInner.style.width).toBe("100%");
+      await waitFor(() => expect(progressBarInner.style.width).toBe("20%"));
+      await waitFor(() => expect(progressBarInner.style.width).toBe("50%"));
+      await waitFor(() => expect(progressBarInner.style.width).toBe("80%"));
+      expect(progressBarInner.style.width).not.toBe("100%");
+    });
   });
 });
